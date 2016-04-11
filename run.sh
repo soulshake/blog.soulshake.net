fd2eb41473d2ebe9544fce5953441f0473bb3043
AJ Bowen <aj@soulshake.net>
2016-04-11 20:05:56 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 #HUGO_THEME
 
 # (set to any value to enable watching)
-# HUGO_WATCH
+HUGO_WATCH=true
 
 # (in seconds, only applies if not watching, if not set, the container will build once and exit)
-#HUGO_REFRESH_TIME
+#HUGO_REFRESH_TIME=10
 
 HUGO_BASEURL="${HUGO_BLOG_URL:=blog.soulshake.net}"
 
@@ -23,7 +23,7 @@ while true
 do
     if [[ $HUGO_WATCH != 'false' ]]; then
         echo "Watching..."
-        $HUGO --verbose server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" || exit 1
+        $HUGO --verbose server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --bind=0.0.0.0 || exit 1
     else
         echo "Building one time..."
         $HUGO --verbose --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" || exit 1
